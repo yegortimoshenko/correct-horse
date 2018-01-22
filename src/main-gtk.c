0de94038ed963b67b6fa42c5803197f78d557339
Tanya Nevskaya <tanyanevskaya@riseup.net>
2018-01-22 21:55:31 +0000

    Redraw application icon

--- src/main-gtk.c
+++ src/main-gtk.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
 
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_default_size(GTK_WINDOW(window), 350, -1);
+  gtk_window_set_icon_name(GTK_WINDOW(window), "correct-horse");
   gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
   gtk_window_set_title(GTK_WINDOW(window), "Correct Horse");
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
