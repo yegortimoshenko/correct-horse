702265805f7c00a9c4c57abc005baee1e302e468
Yegor Timoshenko <yegortimoshenko@riseup.net>
2019-04-24 02:27:06 +0300

    Set up CI

---
+++ pkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs-channels/archive/796a8764ab85746f916e2cc8f6a9a5fc6d4d03ac.tar.gz";
+  sha256 = "1m57gsr9r96gip2wdvdzbkj8zxf47rg3lrz35yi352x1mzj3by3x";
+})
